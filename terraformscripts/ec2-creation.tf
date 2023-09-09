34bc5ecb23119d42ec75af2c3b75ae4593cc0cfa
rahul kothari <139675069+rahulx68@users.noreply.github.com>
2023-09-09 13:38:18 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -4,9 +4,9 @@ region = "ap-south-1"
 }
 
 resource "aws_instance" "AWSServer"{
-ami = " ami-02bb7d8191b50f4bb "
+ami = "ami-02bb7d8191b50f4bb"
 instance_type = "t2.micro"
-key_name = " mithuntechnologiess "
+key_name = "mithuntechnologiess"
 security_groups = ["launch-wizard-2"]
 tags = {
 Name = "tomcatsservers"
