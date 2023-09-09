b98fc3fd2d082db80ae81d3f0c2c3e212cfbfe9a
rahul kothari <139675069+rahulx68@users.noreply.github.com>
2023-09-09 13:33:10 +0530

    Update aws_ec2.tf

--- aws_ec2.tf
+++ aws_ec2.tf
@@ -5,7 +5,7 @@ region = "ap-south-1"
 resource "aws_instance" "AWSServer"{
 ami = "ami-02bb7d8191b50f4bb"
 instance_type = "t2.micro"
-security_groups = {"launch-wizard-2"}
+security_groups = ["launch-wizard-2"]
 key_name = "mithuntechnologiess"
 tags = {
 Name = "tomcatservers"
